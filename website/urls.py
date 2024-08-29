b1766c53d412266cf0e5bf7875db63495a37b6c1
Benjamin <benjaminoheneasare65@gmail.com>
2024-08-29 18:29:32 +0000

    changes

--- website/urls.py
+++ website/urls.py
@@ -3,5 +3,11 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('aboutUs', views.aboutUs, name='aboutUs'),
+    path('blog', views.blog, name='blog'),
+    path('blogDetail', views.blogDetail, name='blogDetail'),
+    path('contact', views. contact, name=' contact'),
+    path('ourWork', views.ourWork, name='ourWork'),
+    path('service', views.service, name='service'),
 ]
--- website/views.py
+++ website/views.py
@@ -3,3 +3,21 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'website/index.html')
+
+def aboutUs(request):
+    return render(request, 'website/about-us.html')
+
+def blog(request):
+    return render(request, 'website/blog.html')
+
+def blogDetail(request):
+    return render(request, 'website/blogDetail.html')
+
+def contact(request):
+    return render(request, 'website/contact-us.html')
+
+def ourWork(request):
+    return render(request, 'website/ourWork.html')
+
+def service(request):
+    return render(request, 'website/service.html')
